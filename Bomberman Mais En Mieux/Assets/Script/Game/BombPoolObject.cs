9fffb45fbb910b33354c6f90033f090d2e0ba0af
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-01-06 16:15:47 +0100

    AAAAH

---
+++ Bomberman Mais En Mieux/Assets/Script/Game/BombPoolObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BombPoolObject : MonoBehaviour
+{
+    public static BombPoolObject instance;
+
+    [SerializeField] private GameObject bombPrefab;
+    private int maxBombOnMap = 5;
+
+    [SerializeField] List<Transform> bombSpawnPointList;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < maxBombOnMap+3; i++) //we create more bombs just in case
+        {
+            GameObject newBomb = Instantiate(bombPrefab);
+            ResetBomb(newBomb);
+        }
+    }
+
+    public void SpawnBomb(GameObject theBomb)
+    {
+        theBomb.transform.position = bombSpawnPointList[Random.Range(0, bombSpawnPointList.Count)].position;
+        theBomb.transform.parent = null;
+        theBomb.SetActive(true);
+    }
+
+    public void ResetBomb(GameObject theBomb)
+    {
+        theBomb.SetActive(false);
+        theBomb.transform.position = Vector3.zero;
+        theBomb.transform.rotation = Quaternion.identity;
+        theBomb.transform.parent = transform;
+    }
+}
