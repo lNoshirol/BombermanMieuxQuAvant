40cd07de67ae3077b07927d051b067624f18cf76
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-01-13 09:04:25 +0100

    State Machine : L'incompréhension

--- Bomberman Mais En Mieux/Assets/Script/Bot/State Machin/AttackState.cs
+++ Bomberman Mais En Mieux/Assets/Script/Bot/State Machin/AttackState.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackState : MonoBehaviour
+public class AttackState : IBotState
 {
-    // Start is called before the first frame update
-    void Start()
+    private BotStateMachine _stateMachine;
+
+    public void OnEnter(BotStateMachine botStateMachine)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnExit(BotStateMachine botStateMachine)
     {
-        
+
     }
 }
--- Bomberman Mais En Mieux/Assets/Script/Bot/State Machin/BotStateMachine.cs
+++ Bomberman Mais En Mieux/Assets/Script/Bot/State Machin/BotStateMachine.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BotStateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public IBotState currentState;
 
-    // Update is called once per frame
-    void Update()
+    public SearchBombState searchBombState = new();
+    public RunAwayState runAwayState = new();
+    public AttackState attackState = new();
+
+    public void ChangeState(IBotState newState)
     {
-        
+        if (currentState != null)
+        {
+            currentState.OnExit(this);
+        }
+
+        currentState = newState;
+        currentState.OnEnter(this);
     }
+}
+
+public interface IBotState
+{
+    public void OnEnter(BotStateMachine botStateMachine);
+
+    public void OnExit(BotStateMachine botStateMachine);
 }
